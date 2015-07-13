880ff91b9f02e3a9d809190a1c399718e4f1ffcf
Jason E. Aten <j.e.aten@gmail.com>
2015-07-13 09:04:49 -0700

    msgpackR: start to upgrade to msgpack version 2. Not yet done.

--- R/unpack.R
+++ R/unpack.R
@@ -54,7 +54,7 @@ function(str) {
 		return(result)
 	}
 
-	.unpack_fixraw <- function() {
+	.unpack_fixstr <- function() {
 		result <- ""
 		result_byte <- list()
 		N <- as.integer(e$.msgpack_data[e$.msgpack_index] & as.raw(0x1F))
@@ -90,12 +90,12 @@ function(str) {
 		return(TRUE)
 	}
 
-	.unpack_float <- function() {
+	.unpack_float32 <- function() {
 		# R supports not "float" but "double".
-		return(.unpack_double())
+		return(.unpack_float64())
 	}
 
-	.unpack_double <- function() {
+	.unpack_float64 <- function() {
 		result <- 0
 		e$.msgpack_index <- e$.msgpack_index + 1
 	
@@ -239,7 +239,82 @@ function(str) {
 		return(result)
 	}
 
-	.unpack_raw16 <- function() {
+
+	.unpack_bin8 <- function() {
+		result <- list()
+		
+		e$.msgpack_index <- e$.msgpack_index + 1		
+        N <- as.integer(e$.msgpack_data[e$.msgpack_index])
+
+        e$.msgpack_index <- e$.msgpack_index + 1
+		
+		for ( i in 1:N ) {
+			result[[i]] <- e$.msgpack_data[e$.msgpack_index]
+			e$.msgpack_index <- e$.msgpack_index + 1
+		}
+		result <- unlist(result)
+		
+		return(result)
+	}
+
+	.unpack_bin16 <- function() {
+		result <- list()
+		N <- 0
+		
+		e$.msgpack_index <- e$.msgpack_index + 1
+		
+		for ( i in seq(8,0,-8) ) {
+			N <- N + as.integer(e$.msgpack_data[e$.msgpack_index])*2^i
+			e$.msgpack_index <- e$.msgpack_index + 1
+		}
+		
+		for ( i in 1:N ) {
+			result[[i]] <- e$.msgpack_data[e$.msgpack_index]
+			e$.msgpack_index <- e$.msgpack_index + 1
+		}
+		result <- unlist(result)
+		
+		return(result)
+	}    
+    
+	.unpack_bin32 <- function() {
+		result <- list()
+		N <- 0
+		
+		e$.msgpack_index <- e$.msgpack_index + 1
+		
+		for ( i in seq(24,0,-8) ) {
+			N <- N + as.integer(e$.msgpack_data[e$.msgpack_index])*2^i
+			e$.msgpack_index <- e$.msgpack_index + 1
+		}
+		
+		for ( i in 1:N ) {
+			result[[i]] <- e$.msgpack_data[e$.msgpack_index]
+			e$.msgpack_index <- e$.msgpack_index + 1
+		}
+		result <- unlist(result)
+		
+		return(result)
+	}
+    
+	.unpack_str8 <- function() {
+		result <- list()
+		
+		e$.msgpack_index <- e$.msgpack_index + 1		
+        N <- as.integer(e$.msgpack_data[e$.msgpack_index])
+
+        e$.msgpack_index <- e$.msgpack_index + 1
+		
+		for ( i in 1:N ) {
+			result[[i]] <- rawToChar(e$.msgpack_data[e$.msgpack_index])
+			e$.msgpack_index <- e$.msgpack_index + 1
+		}
+		result <- .unpack_checkclass(result)
+		
+		return(result)
+	}
+
+	.unpack_str16 <- function() {
 		result <- list()
 		N <- 0
 		
@@ -259,7 +334,7 @@ function(str) {
 		return(result)
 	}
 
-	.unpack_raw32 <- function() {
+	.unpack_str32 <- function() {
 		result <- list()
 		N <- 0
 		
@@ -363,7 +438,7 @@ function(str) {
 		return(result)
 	}
 
-	.unpack_nfixnum <- function() {
+	.unpack_nfixint <- function() {
 		num <- e$.msgpack_data[e$.msgpack_index]
 		e$.msgpack_index <- e$.msgpack_index + 1
 		return(-32 + as.integer(num & as.raw(0x1F)))
@@ -387,11 +462,23 @@ function(str) {
 		return(data)
 	}
 
+    ## todo
+	.unpack_ext8 <- function() { stop("todo: implement .unpack_ext8") }
+	.unpack_ext16 <- function() { stop("todo: implement .unpack_ext16") }
+	.unpack_ext32 <- function() { stop("todo: implement .unpack_ext32") }
+	.unpack_fixext1 <- function() { stop("todo: implement .unpack_fixext1") }
+	.unpack_fixext2 <- function() { stop("todo: implement .unpack_fixext2") }
+	.unpack_fixext4 <- function() { stop("todo: implement .unpack_fixext4") }
+	.unpack_fixext8 <- function() { stop("todo: implement .unpack_fixext8") }
+	.unpack_fixext16 <- function() { stop("todo: implement .unpack_fixext16") }
+
+    ## end todo
+    
 	.unpack_data <- function() {
 		if ( is.null(e$.msgpack_data) ) {
 			return(NULL)
 		}
-		# Positive FixNum
+		# Positive FixInt
 		else if( e$.msgpack_data[e$.msgpack_index] <= as.raw(0x7F) ) {
 			return(.unpack_pfixnum())
 		}
@@ -403,9 +490,9 @@ function(str) {
 		else if ( e$.msgpack_data[e$.msgpack_index] <= as.raw(0x9F) ) {
 			return(.unpack_fixarray())
 		}
-		# FixRaw
+		# FixStr
 		else if ( e$.msgpack_data[e$.msgpack_index] <= as.raw(0xBF) ) {
-			return(.unpack_fixraw())
+			return(.unpack_fixstr())
 		}
 		# nil
 		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xC0) ) {
@@ -422,18 +509,38 @@ function(str) {
 		# true
 		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xC3) ) {
 			return(.unpack_true())
-		}
-		# (reserved)
-		else if ( e$.msgpack_data[e$.msgpack_index] <= as.raw(0xC9) ) {
-			return(NULL)
-		}
-		# float
+          }
+		# bin8
+		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xC4) ) {
+			return(.unpack_bin8())
+          }
+		# bin16
+		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xC5) ) {
+			return(.unpack_bin16())
+          }
+		# bin32
+		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xC6) ) {
+			return(.unpack_bin32())
+          }
+		# ext8
+		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xC7) ) {
+			return(.unpack_ext8())
+          }
+		# ext16
+		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xC8) ) {
+			return(.unpack_ext16())
+          }
+		# ext32
+		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xC9) ) {
+			return(.unpack_ext32())
+          }
+		# float32
 		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xCA) ) {
-			return(.unpack_float())
+			return(.unpack_float32())
 		}
-		# double
+		# float64
 		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xCB) ) {
-			return(.unpack_double())
+			return(.unpack_float64())
 		}
 		# uint8
 		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xCC) ) {
@@ -466,18 +573,38 @@ function(str) {
 		# int64
 		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xD3) ) {
 			return(.unpack_int64())
-		}
-		# (reserved)
+          }
+   		# fixext1
+		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xD4) ) {
+			return(.unpack_fixext1())
+          }
+   		# fixext2
+		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xD5) ) {
+			return(.unpack_fixext2())
+          }
+   		# fixext4
+		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xD6) ) {
+			return(.unpack_fixext4())
+          }
+   		# fixext8
+		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xD7) ) {
+			return(.unpack_fixext8())
+          }
+   		# fixext16
+		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xD8) ) {
+			return(.unpack_fixext16())
+          }
+		# str8
 		else if ( e$.msgpack_data[e$.msgpack_index] <= as.raw(0xD9) ) {
-			return(NULL)
-		}
-		# raw16
+			return(.unpack_str8())
+          }
+		# str16
 		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xDA) ) {
-			return(.unpack_raw16())
+			return(.unpack_str16())
 		}
-		# raw32
+		# str32
 		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xDB) ) {
-			return(.unpack_raw32())
+			return(.unpack_str32())
 		}
 		# array16
 		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xDC) ) {
@@ -495,9 +622,9 @@ function(str) {
 		else if ( e$.msgpack_data[e$.msgpack_index] == as.raw(0xDF) ) {
 			return(.unpack_map32())
 		}
-		# Negative FixNum
+		# Negative FixInt
 		else {
-			return(.unpack_nfixnum())
+			return(.unpack_nfixint())
 		}
 	}
 
---
+++ man/msgpack2notes.txt
@@ -0,0 +1,30 @@
+renames
+-------
+
+fixraw -> fixstr
+raw16 -> str16
+raw32 -> str32
+float -> float32
+double -> float64
+nfixnum -> nfixint
+
+
+add-new-types
+-------------
+
+str8
+
+bin8
+bin16
+bin32
+
+ext8
+ext16
+ext32
+
+fixext1
+fixext2
+fixext4
+fixext8
+fixext16
+
